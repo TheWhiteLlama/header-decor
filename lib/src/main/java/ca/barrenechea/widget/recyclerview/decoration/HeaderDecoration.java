68b80a4d9bce5f7dbdc0848de980ba239cf0da3b
TheWhiteLlama <das.weisse.lama@gmail.com>
2016-06-16 17:56:45 +0200

    - still trying to fix gradle build

--- lib/src/main/java/ca/barrenechea/widget/recyclerview/decoration/HeaderDecoration.java
+++ lib/src/main/java/ca/barrenechea/widget/recyclerview/decoration/HeaderDecoration.java
@@ -1,12 +1,13 @@
+package ca.barrenechea.widget.recyclerview.decoration;
+
 /*
  * Copyright 2016 Christian Ringshofer
- * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
+ *
  * you may not use this file except in compliance with the License.
+ *
  * You may obtain a copy of the License at
- * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,7 +15,6 @@
  * limitations under the License.
  */
 
-package ca.barrenechea.widget.recyclerview.decoration;
 
 import android.content.res.Resources;
 import android.graphics.Canvas;
@@ -363,7 +363,7 @@ public class HeaderDecoration extends RecyclerView.ItemDecoration {
                     final long nextHeaderId = this.getHeaderId(nextAdapterPosition);
                     final long nextSubHeaderId = this.getSubHeaderId(nextAdapterPosition);
 
-                    if (nextSubHeaderId != currentSubHeaderId /*&& nextSubHeaderId != RecyclerView.NO_ID*/) {
+                    if (nextSubHeaderId != currentSubHeaderId) {
 
                         int headersHeight = getHeight(subHeader) + getHeight(getSubHeader(parent, nextAdapterPosition));
                         if (nextHeaderId != currentHeaderId) {
